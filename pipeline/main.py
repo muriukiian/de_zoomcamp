aa53098e85631bcc70deb8a15c17927b970433be
Ian Muriuki <153798623+muriukiian@users.noreply.github.com>
2026-05-18 15:49:26 +0000

    resolving container communication using Docker Networks.

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from pipeline!")
-
-
-if __name__ == "__main__":
-    main()
+docker run -it --rm\
+  --network=pg-network \
+  ingest_data:v001 \
+    --pg_user=admin \
+    --pg_pass=root \
+    --pg_host=pgdatabase \
+    --pg_port=5432 \
+    --pg_db=ny_taxi \
+    --chunksize=100000
+    --target_table=ny_taxi_2021-01 \
